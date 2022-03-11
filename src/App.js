27a936dff69da3d2aa47a11b99884f6204ddb0aa
phamnhutnpx <xuannhutzz@gmail.com>
2022-03-12 00:42:02 +0700

    done code main content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import react, { useState } from 'react';
+import { useState } from 'react';
 import { Spline } from '@splinetool/react-spline';
 import { IoMenu } from "react-icons/io5";
+
+import npxAvt from './images/npxAvt.jpg'
 
 function App() {
   // active icon menu mobile
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <div className="flex w-screen h-screen min-h-screen flex-col relative  pb-20 items-center justify-center bg-primary">
+    <div className="flex w-screen  min-h-screen flex-col relative  pb-20 items-center justify-center bg-primary">
       {/* Navigate */}
       <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
         <div className="w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
@@ -73,8 +75,40 @@ function App() {
       <div className="relative " id="home">
         <Spline scene="https://prod.spline.design/oo6IxFu8UDjFUmjD/scene.spline" />
         {/* <Spline scene="https://draft.spline.design/TMv0D2w0UW1kuudl/scene.spline" className="bg-primary" /> */}
-
+        <div className="absolute w-full justify-center items-center bottom-10 flex">
+          <div className="shadow-md p-4 flex items-center bg-zinc-900 rounded-3xl">
+            <p className="text-textBase">You can rotate it</p>
+          </div>
+        </div>
       </div>
+
+      {/* Main */}
+      <main className="w-[80%] mt-5">
+        {/* Section */}
+        <section className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24">
+          {/* img box */}
+          <div className="w-full h-420 flex items-center justify-center">
+            <div className="w-275 h-340 bg-slate-100 rounded-md relative">
+              <img className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg drop-shadow-2xl"
+                src={npxAvt}
+                alt=""
+              />
+            </div>
+          </div>
+          {/* text content */}
+          <div className="w-full h-420 flex flex-col items-center justify-center" >
+            <p className="text-lg text-textBase text-center">
+              lorem ipsum dolor sit amet, consectet lorem ipsum dolor sit amet, lobortis lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum
+              lorem ipsum dolor sit amet, consectet lorem ipsum dolor sit amet, lobortis lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum
+            </p>
+            <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
+              <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Download
+              </span>
+            </button>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
